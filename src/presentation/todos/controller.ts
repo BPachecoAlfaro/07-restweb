c9864c7850f090ecc1ba5a5be78a56301e856519
Bpachecoalfaro <bpachecoalfaro@gmail.com>
2024-03-03 21:59:15 -0300

    seccion 15 terminada

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { prisma } from "../../data/postgres";
+import { CreateTodoDto, UpdateTodoDto } from "../../domain/dtos";
 
 
 export class TodosController {
@@ -30,11 +31,11 @@ export class TodosController {
     }
 
     public createTodo = async ( req: Request, res: Response ) => {
-        const { text } = req.body;
-        if ( !text) return res.status(400).json({ error: 'Text property is required' });
+        const [error, createTodoDto] = CreateTodoDto.create( req.body );
+        if ( error ) return res.status(400).json({ error })
 
         const todo = await prisma.todo.create({
-            data: { text }
+            data: createTodoDto!
         })
 
         res.json( todo );
@@ -43,22 +44,26 @@ export class TodosController {
     public updateTodo = async( req: Request, res: Response ) => {
 
         const id = +req.params.id;
-        if ( isNaN( id ) ) return res.status(400).json( { eror: 'ID argument is not a number'})
-        const { text, completedAt } = req.body;
+        const [error, updateTodoDto ] = UpdateTodoDto.create({...req.body, id});
+        if ( error ) return res.status(400).json({ error });
 
-        const todoById = await prisma.todo.update({
+        const todo = await prisma.todo.findFirst({
             where: {
-                id: id
-            },
-            data: {
-                text: text,
-                completedAt: (completedAt) ? new Date(completedAt) : null,
+                id
             }
         });
 
-        if ( !todoById ) return res.status(404).json( { error: `Todo with id ${ id } not found`} )
+        if ( !todo ) return res.status(404).json( { error: `Todo with id ${ id } not found`} )
 
-        res.json( todoById );
+        const updatedTodo = await prisma.todo.update({
+            where: {
+                id: id
+            },
+            data: updateTodoDto!.values
+        });
+
+
+        res.json( updatedTodo );
 
     };
 
